59c7dfb7a6be1ae09340743218b94027b512be84
leoallvez <leoallvez@hotmail.com>
2017-10-05 20:01:13 -0300

    seed de vinculo da Pam

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder {
         $this->call(UsersTableSeeder::class);
         $this->call(UsuarioNiveisTableSeeder::class);
         $this->call(VinculoTableSeeder::class);
+        $this->call(AdotanteAdotivoTableSeeder::class);
     }
 }
