25feb474e040bcf9cfa3747d75bcdd444e2e7121
leoallvez <leoallvez@hotmail.com>
2018-03-19 00:08:13 -0300

    trabalhando na responsividade da listagem de adotivos

--- resources/views/adotivo/index.blade.php
+++ resources/views/adotivo/index.blade.php
@@ -36,7 +36,7 @@
       </div>
       <div class="clearfix"></div>
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
           <div class="x_panel">
             <div class="x_title">
               <h2>Lista</h2>
